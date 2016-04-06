855843ff6e345ac39b17c0aeb63eb8c16fd97534
Jit <kanetkarster@gmail.com>
2016-04-06 11:52:06 -0400

    finished opencv setup

--- ECSE415-Project/main.cpp
+++ ECSE415-Project/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#include <opencv2/opencv.hpp>
+
 int main() {
 	std::cout << "Hello World" << std::endl;
+	cv::Mat img = cv::imread("test.png");
+	imshow("test", img);
+	cv::waitKey();
 }

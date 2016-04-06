b0ce84ae18a8b0214c31011d651334197fed27d8
Jit <kanetkarster@gmail.com>
2016-04-06 12:45:15 -0400

    added some comments

--- ECSE415-Project/lbp-face.cpp
+++ ECSE415-Project/lbp-face.cpp
@@ -15,15 +15,18 @@ void get_lbp_representation(cv::Mat &const img , cv::Mat &lbp) {
 	/* fill lbp matrix */
 	for (int row=1; row<img.rows-1; row++) {
 		for (int col=1; col<img.cols-1; col++) {
+			/* calculate LBP descriptor */
+
 			int center = img.at<int>(row, col);
 			unsigned char binary_pattern = 0;
-			/*	 _ _ _ _
+			/* Grid of neighboring pixels
+			 *	 _ _ _ _
 			 *	| 1 2 3 |
 			 *	| 8 * 4 |
 			 *	| 7 6 5 |
 			 *   - - - -
 			 */
-
+			/* set bits in accordance with above grid */
 			binary_pattern |= (img.at<int>(row-1, col-1) > center) << 7;	// 1
 			binary_pattern |= (img.at<int>(row-1, col) > center) << 6;		// 2
 			binary_pattern |= (img.at<int>(row-1, col+1) > center) << 5;	// 3
@@ -33,6 +36,7 @@ void get_lbp_representation(cv::Mat &const img , cv::Mat &lbp) {
 			binary_pattern |= (img.at<int>(row+1, col-1) > center) << 1;	// 7
 			binary_pattern |= (img.at<int>(row, col-1) > center) << 0;		// 8
 
+			/* set data in output Matrix */
 			lbp.at<unsigned char>(row, col) = binary_pattern;
 		}
 	}
